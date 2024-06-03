b588cc4dcd42334e7a52ad1a1b0b808dfdfbe8fa
Roman Kornienko <kornienkoroman86@gmail.com>
2024-06-03 18:47:47 +0200

    added space to bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ API_TOKEN = '7124611334:AAGo85HXf52joAU_1dQO-b0V79x1nTO4tuY'
 
 bot = telebot.TeleBot(API_TOKEN, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
